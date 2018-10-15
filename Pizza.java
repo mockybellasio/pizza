24c2bcefcf84521eaf30eba1f9845c4c01790d8d
mockybellasio <43879122+mockybellasio@users.noreply.github.com>
2018-10-15 14:21:33 +0200

    Add files via upload

---
+++ Pizza.java
@@ -0,0 +1,61 @@
+package com.pizza;
+
+public class Pizza {
+	
+	
+	String code;
+	String designation;
+	float Prix;
+	
+			
+		
+	public Pizza(String code, String designation, float prix) {
+		super();
+		this.code = code;
+		this.designation = designation;
+		Prix = prix;
+	}
+
+
+	
+
+
+
+
+
+	@Override
+	public String toString() {
+		return "PizzaMemDao [code=" + code + ", designation=" + designation + ", Prix=" + Prix + "]";
+	}
+
+
+
+
+
+
+
+
+	public String getCode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+	
+	
+
+
