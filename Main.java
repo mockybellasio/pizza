5f4b09e798b5ebeeff8dfa2717540d746bb8795b
mockybellasio <43879122+mockybellasio@users.noreply.github.com>
2018-10-15 15:12:47 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,11 +4,11 @@ public class Main {
 
 	public static <tabPizza> void main(String[] args) {
 		
-		Pizza pizza1 = new Pizza("Marguerita", "mar", 10, Modele.AUTRE);
+		Pizza pizza1 = new Pizza("Marguerita", "mar", 10);
 		
 		System.out.println(pizza1);
 		
-		Pizza pizza2= new Pizza("3 fromages", "fro", 12, Modele.FROMAGE);
+		Pizza pizza2= new Pizza("3 fromages", "fro", 12);
    
 		  System.out.println(pizza2);
 
