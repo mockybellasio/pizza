0aab2e7d80c53f81ae8d4b240d86b1f9507a34b2
mockybellasio <43879122+mockybellasio@users.noreply.github.com>
2018-10-15 14:22:28 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.pizza;
+
+public class Main {
+
+	public static <tabPizza> void main(String[] args) {
+		
+	   PizzaemDao pizz = new PizzaemDao();
+	   
+		System.out.println(pizz);
+		
+		
+
+	}
+
+}
